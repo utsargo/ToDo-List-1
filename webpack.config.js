2965fa279535553fedabd97ab3684e8fe208c344
PrantoshB <pbiswas1007@gmail.com>
2022-12-07 14:56:22 +0530

    Build: Create To-Do List Structure using HTML and CSS

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+ /* eslint-disable */
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "none",
+  entry: {
+    index: "./src/index.js",
+  },
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
